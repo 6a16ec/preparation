e56d6929f8a0c1b5808908666edf4fa8d306d464
6a16ec <semaev2014@yandex.ru>
2019-03-26 18:35:01 +0300

    Update echo.py

--- examples/aiogram/echo.py
+++ examples/aiogram/echo.py
@@ -1,16 +1,15 @@
-import asyncio
+import logging
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from config import tg_token, proxy
-import sys
+from config import tg_token, proxy, proxy_auth
 
-
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token=tg_token) if proxy is None else \
 Bot(token=tg_token, proxy=proxy) if proxy_auth is None else \
 Bot(token=tg_token, proxy=proxy, proxy_auth=proxy_auth)
-
 dp = Dispatcher(bot)
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -20,4 +19,5 @@ async def send_welcome(message: types.Message):
 async def send_welcome(message: types.Message):
     await message.reply(message.text, reply=False)
 
-executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+	executor.start_polling(dp, skip_updates=True)
